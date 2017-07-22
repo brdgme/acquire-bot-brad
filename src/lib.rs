ccd86d1e6504949b2cc07924be3bffd60b9b78ec
Michael Alexander <beefsack@gmail.com>
2017-07-22 21:17:32 +1000

    Initial work on play command quality for corp creation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 extern crate acquire;
 extern crate brdgme_game;
 
-use acquire::{Game, Phase, PubState, CanEnd};
+use acquire::{Game, Phase, PlayerState, CanEnd};
+use acquire::board::{Loc, Tile};
 use brdgme_game::command::Spec as CommandSpec;
 use brdgme_game::bot::{Botter, BotCommand};
 
@@ -11,20 +12,20 @@ impl Botter<Game> for Brad {
     fn commands(
         &mut self,
         player: usize,
-        pub_state: &PubState,
+        player_state: &PlayerState,
         players: &[String],
         command_spec: &CommandSpec,
         game_id: Option<String>,
     ) -> Vec<BotCommand> {
-        if pub_state.phase.whose_turn() != player {
+        if player_state.public.phase.whose_turn() != player {
             return vec![];
         }
-        if pub_state.can_end() == CanEnd::True {
+        if player_state.public.can_end() == CanEnd::True {
             // Always end it if we can, as it's so rarely advantageous not to.
             return vec!["end".into()];
         }
-        match pub_state.phase {
-            Phase::Play(_) => handle_play_phase(player, pub_state),
+        match player_state.public.phase {
+            Phase::Play(_) => handle_play_phase(player_state),
             Phase::Buy { remaining, .. } => unimplemented!(),
             Phase::Found { at, .. } => unimplemented!(),
             Phase::ChooseMerger { at, .. } => unimplemented!(),
@@ -39,18 +40,26 @@ impl Botter<Game> for Brad {
     }
 }
 
-fn handle_play_phase(player: usize, pub_state: &PubState) -> Vec<BotCommand> {
-    let available_corps = pub_state.board.available_corps();
+fn handle_play_phase(player_state: &PlayerState) -> Vec<BotCommand> {
+    let available_corps = player_state.public.board.available_corps();
     // Consider each tile and give a quality score for each.
-    let priv_state = pub_state
-        .priv_state
-        .expect("expected to be able to view private state");
     let mut commands: Vec<BotCommand> = vec![];
-    for t in &priv_state.tiles {
-        let neighbouring_corps = pub_state.board.neighbouring_corps(t);
-        if pub_state.board.loc_neighbours_multiple_safe_corps(t) {
+    for t in &player_state.tiles {
+        let neighbouring_corps = player_state.public.board.neighbouring_corps(t);
+        if player_state.public.board.loc_neighbours_multiple_safe_corps(t) {
             // This tile is unplayable, ignore it.
             continue;
+        }
+        if t.neighbours().iter().find(|n| player_state.public.board.get_tile(*n) == Tile::Unincorporated).is_some() {
+            // Founding location
+            if available_corps.is_empty() {
+                // Can't found anything at the moment.
+                continue;
+            }
+            commands.push(BotCommand {
+                quality: 200,
+                commands: vec![format!("play {}", t)],
+            });
         }
     }
     commands
